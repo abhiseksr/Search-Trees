87b84abf9b99abea9ad423f7f07f517f25b48074
abhishek <abhishekkumartbbt@gmail.com>
2022-02-28 20:58:40 +0530

    Added bsTree.cpp and added comment in avlTree.cpp

--- bsTree.cpp
+++ bsTree.cpp
@@ -1,28 +1,26 @@
-/* AVL tree implementation in C/C++
+/* Binary Search tree implementation in C/C++
 Author - Abhishek Kumar */
 #include <iostream>
 using namespace std;
 
-class AVL
+class BST
 {
 public:
-    static class AVL *root;
-    AVL *left, *right, *par;
-    int data, depth;
+    static class BST *root;
+    BST *left, *right, *par;
+    int data;
 
-    AVL *create(int data)
+    BST *create(int data)
     {
-        AVL *newnode = (AVL *)malloc(sizeof(AVL));
+        BST *newnode = (BST *)malloc(sizeof(BST));
         newnode->data = data;
         newnode->left = 0;
         newnode->right = 0;
         newnode->par = 0;
-        newnode->depth = 1; //depth of a newnode shall be 1 and not zero to \
-        differentiate between no child (which return 0) and having child
         return newnode;
     }
 
-    void dfs(AVL *head = root)
+    void dfs(BST *head = root)
     {
         if (root == 0)
             return;
@@ -37,128 +35,15 @@ public:
         }
     }
 
-    void right_rotation(AVL *x)
-    {
-        AVL *y = x->left;
-        x->left = y->right;
-        if (y->right != 0)
-        {
-            y->right->par = x;
-        }
-        if (x->par != 0 && x->par->right == x)
-        {
-            x->par->right = y;
-        }
-        else if (x->par != 0 && x->par->left == x)
-        {
-            x->par->left = y;
-        }
-        y->par = x->par;
-        y->right = x;
-        x->par = y;
-    }
-
-    void left_rotation(AVL *x)
-    {
-        AVL *y = x->right;
-        x->right = y->left;
-        if (y->left != 0)
-        {
-            y->left->par = x;
-        }
-        if (x->par != 0 && x->par->left == x)
-        {
-            x->par->left = y;
-        }
-        else if (x->par != 0 && x->par->right == x)
-        {
-            x->par->right = y;
-        }
-        y->par = x->par;
-        y->left = x;
-        x->par = y;
-    }
-
-    int depthf(AVL *node)
-    {
-        if (node == 0)
-            return 0;
-        return node->depth;
-    }
-
-
-    // draw the initial and final graph of each cases(take case where every node has two child)\
-    and update the nodes depth before rotation
-    void helper(AVL *node)
-    {
-        if (depthf(node->left) - depthf(node->right) > 1)
-        {
-            if (depthf(node->left->left) > depthf(node->left->right))
-            {
-                node->depth = max(depthf(node->right)+1,depthf(node->left->right)+1);
-                node->left->depth = max(depthf(node->left->left)+1, depthf(node)+1);
-                right_rotation(node);
-                
-            }
-            else
-            {
-                node->left->depth = max(depthf(node->left->left)+1,depthf(node->left->right->left)+1);
-                node->depth = max(depthf(node->right)+1,depthf(node->left->right->right)+1);
-                node->left->right->depth = max(depthf(node)+1,depthf(node->left)+1);
-                left_rotation(node->left);
-                right_rotation(node);
-            }
-        }
-        else if (depthf(node->left) - depthf(node->right) < -1)
-        {
-            if (depthf(node->right->right) > depthf(node->right->left))
-            {
-                node->depth = max(depthf(node->left)+1,depthf(node->right->left)+1);
-                node->right->depth = max(depthf(node->right->right)+1, depthf(node)+1);
-                left_rotation(node);
-            }
-            else
-            {
-                node->right->depth = max(depthf(node->right->right)+1,depthf(node->right->left->right)+1);
-                node->depth = max(depthf(node->left)+1,depthf(node->right->left->left)+1);
-                node->right->left->depth = max(depthf(node)+1,depthf(node->right)+1);
-                right_rotation(node->right);
-                left_rotation(node);
-            }
-        }
-    }
-
-    void balance(AVL *node)
-    {
-        while (node != root)
-        {
-            int d = node->depth;
-            node = node->par;
-            if (node->depth < d + 1)
-            {
-                node->depth = d + 1;
-            }
-            if (node==root && depthf(node->left) - depthf(node->right) > 1)
-            {
-                root = node->left;
-            }
-            else if (node==root && depthf(node->left) - depthf(node->right) < -1)
-            {
-                root = node->right;
-            }
-            helper(node);
-        }
-    }
-
     void insert(int data)
     {
-        AVL *newnode = create(data);
+        BST *newnode = create(data);
         if (root == 0)
         {
             root = newnode;
             return;
         }
-        AVL *temp = root, *prev = 0;
+        BST *temp = root, *prev = 0;
         while (temp != 0)
         {
             prev = temp;
@@ -184,9 +69,8 @@ public:
             prev->right = newnode;
         }
         newnode->par = prev;
-        balance(newnode);
     }
-    AVL *inorderPredecessor(AVL *head){
+    BST *inorderPredecessor(BST *head){
         if (head==0) return head;
         while (head->right!=0){
             head = head->right;
@@ -194,7 +78,7 @@ public:
         return head;
     }
 
-    AVL *inorderSuccessor(AVL *head){
+    BST *inorderSuccessor(BST *head){
         if (head==0) return head;
         while (head->left!=0){
             head = head->left;
@@ -202,8 +86,8 @@ public:
         return head;
     }
 
-    void remove(int data,AVL *temp = root){
-        AVL *prev = 0;
+    void remove(int data,BST *temp = root){
+        BST *prev = 0;
         while (temp!=0 && temp->data!=data){
             prev = temp;
             if (data<temp->data){
@@ -216,8 +100,8 @@ public:
         if (temp->data!=data){
             return;
         }
-        AVL *l = inorderPredecessor(temp->left);
-        AVL *r = inorderSuccessor(temp->right);
+        BST *l = inorderPredecessor(temp->left);
+        BST *r = inorderSuccessor(temp->right);
         if (l==0 && r==0){
             if (prev==0){
                 root = 0;
@@ -230,25 +114,20 @@ public:
                     prev->right = 0;
                 }
                 free(temp);
-                balance(prev);
             }
             return;
         }
-        // while going upward from this node till root - we will keep balancing
-        AVL *start;
         if (l!=0){
             if (l==temp->left){
                 l->right = temp->right;
                 if (l->right!=0){
                     l->right->par = l;
                 }
-                start = l;
             }
             else{
                 if (l->left!=0){
                     l->left->par = l->par;
                 }
-                start = l->par;
                 l->par->right = l->left;
                 l->right = temp->right;
                 l->par = 0;
@@ -273,7 +152,6 @@ public:
                 }
                 free(temp);
             }
-            balance(start);
             return;
         }
         else{
@@ -282,13 +160,11 @@ public:
                 if (r->left!=0){
                     r->left->par = r;
                 }
-                start = r;
             }
             else{
                 if (r->right!=0){
                     r->right->par = r->par;
                 }
-                start = r->par;
                 r->par->left = r->right;
                 r->left = temp->left;
                 r->par = 0;
@@ -312,12 +188,11 @@ public:
                 }
                 free(temp);
             }
-            balance(start);
             return;
         }
     }
     bool search(int data){
-        AVL *temp = root;
+        BST *temp = root;
         while (temp!=0 && temp->data!=data){
             if (data<temp->data){
                 temp = temp->left;
@@ -333,12 +208,12 @@ public:
     }
 };
 
-AVL* AVL::root = 0;
+BST* BST::root = 0;
 
 int main()
 {
 
-    AVL s;
+    BST s;
     s.insert(14);
     s.insert(17);
     s.insert(11);
